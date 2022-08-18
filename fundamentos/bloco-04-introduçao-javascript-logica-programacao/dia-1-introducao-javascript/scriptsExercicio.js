da684421e67b1660c1e16d183f221f48d7a7ada9
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-18 18:44:55 -0300

    resolvendo requisito 1

---
+++ fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scriptsExercicio.js
@@ -0,0 +1,17 @@
+//exercicio 1
+let a = 10;
+let b = 2;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
+
+//exercicio 2
