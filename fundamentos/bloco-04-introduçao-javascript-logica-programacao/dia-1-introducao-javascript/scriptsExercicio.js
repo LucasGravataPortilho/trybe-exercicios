62c37825f6e27fcc1809085c3657a194fc6c3038
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-18 19:49:23 -0300

    requisito 7

--- fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scriptsExercicio.js
+++ fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scriptsExercicio.js
@@ -62,7 +62,7 @@ if (g + h > i && h + i > g && g + i > h && soma === 180) {
 //exercicio 6
 let peça = "rainha";
 
-switch (peça.) {
+switch (peça) {
     case "peao":
         console.log("frente e diagonal");
         break;
@@ -89,4 +89,23 @@ switch (peça.) {
 
     default:
         console.log("não é uma peça de xadrez!");
+}
+
+//exercicio 7
+const nota = 81;
+
+if (nota >= 90 && nota <= 100) {
+    console.log("A");
+} else if (nota < 90 && nota >= 80) {
+    console.log("B");
+} else if (nota < 80 && nota >= 70) {
+    console.log("C");
+} else if (nota < 70 && nota >= 60) {
+    console.log("D");
+} else if (nota < 60 && nota >= 50) {
+    console.log("E");
+} else if (nota < 50 && nota >= 0) {
+    console.log("F");
+} else {
+    console.log("erro!");
 }
