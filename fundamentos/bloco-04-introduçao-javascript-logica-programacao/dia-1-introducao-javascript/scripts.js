65ca7461d84344d7e1d8bf12c338092ce565b14b
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-18 12:02:07 -0300

    exercitando variáveis do javascript

---
+++ fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scripts.js
@@ -0,0 +1,11 @@
+let age = 20;
+let name = "Hulk";
+let fullName = "Lucas Gravatá Portilho";
+// camelCase (boa prática da programação).
+
+console.log(age);
+console.log(name);
+console.log(fullName);
+// finalizando exercicio da videoaula
+
+// iniciando exercicio do foguete de variáveis.
