32eb65663e5a028e386607f7ff21090126603ea0
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-18 12:48:08 -0300

    exercitando tipos primitivos e operações aritméticas

--- fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scripts.js
+++ fundamentos/bloco-04-introduçao-javascript-logica-programacao/dia-1-introducao-javascript/scripts.js
@@ -17,3 +17,33 @@ console.log(myName);
 console.log(birthCity);
 console.log(birthYear);
 // finalizando exercicio do foguete.
+
+// iniciando tipos primitivos, tipagem dinamica e operações aritmeticas.
+
+// let movie = "Avengers"; //string literal
+// let score = 10; //number literal
+// let isValid = true; //boolean
+// let firstName; //undefined
+// let color = null; //redefinição de valor
+
+let salary = 3500;
+console.log(salary + salary);
+
+// exercicio de tipos
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+    firstName: "Ana",
+    lastName: "Santos",
+};
+const patientEmail = "ana@email.com";
+
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = (2 * base) + (2 * height);
+
+console.log(area);
+console.log(perimeter);
+// finalizando tipos primitivos
+
