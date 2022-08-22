1a9659a2d9dab2f9db600f37680ef93c98a1dbe2
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-22 15:28:09 -0300

    adicionando string invertida

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -7,3 +7,13 @@ for (let i = 1; i <= 10; i += 1) {
 console.log(fatorial);
 //finalizando exercicio fatorial 10
 
+let word = "tryber";
+function reverseString (str) {
+    let splitString = str.split("");
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+    console.log(joinArray); 
+}
+
+reverseString("tryber");
+// invertendo string
