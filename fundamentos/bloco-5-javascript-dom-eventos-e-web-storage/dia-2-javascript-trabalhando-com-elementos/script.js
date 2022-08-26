4bfe95d2a236df2a7c3e610f3e097b7e67f3d615
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-26 09:57:39 -0300

    adicionando exercicios de fixação

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -0,0 +1,7 @@
+// arquivo script.js
+
+console.log(document.getElementById('start').nextSibling) // nó
+
+console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
+
+console.log(document.querySelector('#start')) 
