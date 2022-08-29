694aa086f41ba355febdaaf96f9188bc61abf21d
Lucas Portilho <lucasportilho18@gmail.com>
2022-08-29 19:31:08 -0300

    adicionando Exercicio 1

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,22 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+  let decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  let decemberDays = document.querySelector('#days');
+
+  for (let index2 = 0; index2 < decemberDaysList.length; index2 += 1) {
+    const dias = decemberDaysList[index2];
+    const listaDias = document.createElement('li');
+    listaDias.innerHTML = dias;
+    listaDias.className = 'day';
+
+    if(listaDias.innerText.includes('24') || listaDias.innerText.includes('25') || listaDias.innerText.includes('31')) {
+        listaDias.classList.add('holiday');
+    }
+
+    if(listaDias.innerText == '4' || listaDias.innerText == '11' || listaDias.innerText =='18' || listaDias.innerText =='25') {
+        listaDias.classList.add('friday');
+    }
+
+    decemberDays.appendChild(listaDias);
+  };
