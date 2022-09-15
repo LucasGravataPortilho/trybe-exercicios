a73b09053dd908c2087f19acf5b6b568d8406357
Lucas Portilho <lucasportilho18@gmail.com>
2022-09-15 15:20:13 -0300

    resolvendo exercicio 2, array em ordem crescente

--- bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1--javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1--javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -22,3 +22,20 @@ const testingScope = (escopo) => {
 //     }
 //     console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
 //   }
+
+// ----------------------------------------------------------------------
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = () => {
+
+oddsAndEvens.sort((a,b) => {
+    if (a > b) return 1;
+    if (a < b) return -1;
+    return 0;
+});
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+}
+
+sortOddsAndEvens();
