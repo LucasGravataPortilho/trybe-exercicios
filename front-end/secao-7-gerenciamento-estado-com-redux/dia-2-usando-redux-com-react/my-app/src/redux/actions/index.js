a18a803893bb422241c485f0fc61525a043d5314
Lucas Portilho <lucasportilho18@gmail.com>
2022-11-30 15:15:49 -0300

    imutabilidade do estado global

--- front-end/secao-7-gerenciamento-estado-com-redux/dia-2-usando-redux-com-react/my-app/src/redux/actions/index.js
+++ front-end/secao-7-gerenciamento-estado-com-redux/dia-2-usando-redux-com-react/my-app/src/redux/actions/index.js
@@ -3,3 +3,7 @@ export const actionCreator = (increment = 1) => ({
     type: 'INCREMENT_COUNTER',
     payload: increment,
   });
+
+export const clickCounter = () => ({
+    type: 'INCREMENT_CLICK',
+});
